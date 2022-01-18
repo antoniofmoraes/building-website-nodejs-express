e84830112293cb778bcd883a41ebcfb6c4509a4e
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2022-01-18 12:36:18 -0300

    lesson 03-04

--- routes/speakers.js
+++ routes/speakers.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = () => {
-    router.get('/', (req, res) => res.send('Speakers list'));
+module.exports = (params) => {
+    const { speakersService } = params;
+
+    router.get('/', async (req, res) => {
+        const speakers = await speakersService.getList();
+        return res.json(speakers);
+    });
 
     router.get('/:shortname', (req, res) => res.send(`Details page of ${req.params.shortname}`));
 
