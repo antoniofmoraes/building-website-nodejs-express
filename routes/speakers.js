63834d4115e5bfa32fdf671afcb83fde34ed967f
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2022-01-20 11:33:59 -0300

    lessons 04-06 and 07

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,10 +7,16 @@ module.exports = (params) => {
 
     router.get('/', async (req, res) => {
         const speakers = await speakersService.getList();
+        speakers.artworks = await speakersService.getAllArtwork();
         res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
     });
 
-    router.get('/:shortname', (req, res) => res.send(`Details page of ${req.params.shortname}`));
+    router.get('/:shortname', async (req, res) => {
+        const speaker = await speakersService.getSpeaker(req.params.shortname);
+        speaker.artworks = await speakersService.getArtworkForSpeaker(speaker.shortname);
+        console.log(speaker);
+        res.render('layout', { pageTitle: speaker.name, template: 'speakers-detail', speaker });
+    });
 
     return router;
 };
