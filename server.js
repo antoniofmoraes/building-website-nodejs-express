91b553854fb80dfd54729badd73a25fc02c18e27
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2022-01-21 11:27:27 -0300

    lessons 05-02 and 03

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
+const createError = require('http-errors');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
@@ -48,6 +49,17 @@ app.use(
         speakersService,
     })
 );
+
+app.use((req, res, next) => next(createError(404, 'File not nound')));
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    console.error(err);
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.status(status);
+    res.render('error');
+});
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
